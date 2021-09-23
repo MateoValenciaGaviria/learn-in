f6ce0058d1ea3d29158a75a73938266933dc6438
MateoValenciaGaviria <mateo.valencia.g@gmail.com>
2021-09-23 18:34:41 -0500

    Panel grids and selectors added

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,15 +1,66 @@
 import React from 'react';
 
 export const Home = () => {
+
   return (
     <div className="home">
       <div className="home__leftcontainer">
-        <div className="home__publicitycontainer"></div>
+        <div className="home__publicitycontainer">
+
+        </div>
         <div className="home__modulescontainer">
-          <div className="home__box"></div>
-          <div className="home__box"></div>
-          <div className="home__box"></div>
-          <div className="home__box"></div>
+          <div className="home__panelcontainer">
+            <div className="home__selectcontainer">
+              <select name="panels" id="panels" className="home__select">
+                <option value="select" disabled selected className="home__selectoption--disable">Selecciona un panel</option>
+                <option value="exams" className="home__selectoption">Exámenes próximos</option>
+                <option value="activities" className="home__selectoption">Actividades próximas</option>
+                <option value="progress" className="home__selectoption">Progreso del curso</option>
+                <option value="tasks" className="home__selectoption">Lista de tareas</option>
+                <option value="messages" className="home__selectoption">Últimos mensajes</option>
+                <option value="playlist" className="home__selectoption">Lista de reproducción</option>
+              </select>
+            </div>
+          </div>
+          <div className="home__panelcontainer">
+            <div className="home__selectcontainer">
+              <select name="panels" id="panels" className="home__select">
+                <option value="select" disabled selected className="home__selectoption--disable">Selecciona un panel</option>
+                <option value="exams" className="home__selectoption">Exámenes próximos</option>
+                <option value="activities" className="home__selectoption">Actividades próximas</option>
+                <option value="progress" className="home__selectoption">Progreso del curso</option>
+                <option value="tasks" className="home__selectoption">Lista de tareas</option>
+                <option value="messages" className="home__selectoption">Últimos mensajes</option>
+                <option value="playlist" className="home__selectoption">Lista de reproducción</option>
+              </select>
+            </div>
+          </div>
+          <div className="home__panelcontainer">
+            <div className="home__selectcontainer">
+              <select name="panels" id="panels" className="home__select">
+                <option value="select" disabled selected className="home__selectoption--disable">Selecciona un panel</option>
+                <option value="exams" className="home__selectoption">Exámenes próximos</option>
+                <option value="activities" className="home__selectoption">Actividades próximas</option>
+                <option value="progress" className="home__selectoption">Progreso del curso</option>
+                <option value="tasks" className="home__selectoption">Lista de tareas</option>
+                <option value="messages" className="home__selectoption">Últimos mensajes</option>
+                <option value="playlist" className="home__selectoption">Lista de reproducción</option>
+              </select>
+            </div>
+          </div>
+          <div className="home__panelcontainer">
+            <div className="home__selectcontainer">
+              <select name="panels" id="panels" className="home__select">
+                <option value="select" disabled selected className="home__selectoption--disable">Selecciona un panel</option>
+                <option value="exams" className="home__selectoption">Exámenes próximos</option>
+                <option value="activities" className="home__selectoption">Actividades próximas</option>
+                <option value="progress" className="home__selectoption">Progreso del curso</option>
+                <option value="tasks" className="home__selectoption">Lista de tareas</option>
+                <option value="messages" className="home__selectoption">Últimos mensajes</option>
+                <option value="playlist" className="home__selectoption">Lista de reproducción</option>
+              </select>
+            </div>
+          </div>
         </div>
       </div>
       <div className="home__rightcontainer">
