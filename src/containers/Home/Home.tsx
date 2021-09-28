8fac5ef34a534d1b26539533716d341d55186a35
MateoValenciaGaviria <mateo.valencia.g@gmail.com>
2021-09-28 14:25:10 -0500

    Sort panels function added

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -2,51 +2,89 @@ import React, { useState } from 'react';
 
 export const Home = () => {
 
-  var currentPanel1: string = "";
-  var currentPanel2: string = "";
-  var currentPanel3: string = "";
-  var currentPanel4: string = "";
+  var currentPanel: string = "";
 
   const  [panel1, setpanel1] = useState("select");
   const  [panel2, setpanel2] = useState("select");
   const  [panel3, setpanel3] = useState("select");
   const  [panel4, setpanel4] = useState("select");
 
-  const handlePanel = (numberPanel:number, selectedPanel: string) => {
-    switch (numberPanel) {
-      case 1:
-        currentPanel1 = selectedPanel;
-        break;
-      case 2:
-        currentPanel2 = selectedPanel;
-        break;
-      case 3:
-        currentPanel3 = selectedPanel;
-        break;
-      case 4:
-        currentPanel4 = selectedPanel;
-        break;
-    }
+  const handleSelectedPanel = (numberPanel:number, selectedPanel: string) => {
+    //Sets the current panel selected
+    currentPanel = selectedPanel;
+    sortPanels(numberPanel);
   }
 
-  function sortPanels(){
-    var indexPanelChanged:number = 0;
-
-    if(currentPanel1 !== ""){
-      indexPanelChanged = 1;
-    }else if(currentPanel2 !== ""){
-      indexPanelChanged = 2;
-    }else if(currentPanel3 !== ""){
-      indexPanelChanged = 3;
-    }else{
-      indexPanelChanged = 4;
+  function sortPanels(indexPanelChanged: number){
+    
+    //Sets a new panel if the container is empty
+    //Sorts the panels if the user change its order
+    switch (indexPanelChanged) {
+      case 1:
+        switch (currentPanel) {
+          case panel2:
+            setpanel2(panel1);
+            break;
+          case panel3:
+            setpanel3(panel1);
+            break;
+          case panel4:
+            setpanel4(panel1);
+            break;
+        }
+        setpanel1(currentPanel);
+        break;
+      case 2:
+        switch (currentPanel) {
+          case panel1:
+            setpanel1(panel2);
+            break;
+          case panel3:
+            setpanel3(panel2);
+            break;
+          case panel4:
+            setpanel4(panel2);
+            break;
+        }
+        setpanel2(currentPanel);
+        break;
+      case 3:
+        switch (currentPanel) {
+          case panel1:
+            setpanel1(panel3);
+            break;
+          case panel2:
+            setpanel2(panel3);
+            break;
+          case panel4:
+            setpanel4(panel3);
+            break;
+        }
+        setpanel3(currentPanel);
+        break;
+      case 4:
+        switch (currentPanel) {
+          case panel1:
+            setpanel1(panel4);
+            break;
+          case panel2:
+            setpanel2(panel4);
+            break;
+          case panel3:
+            setpanel3(panel4);
+            break;
+        }
+        setpanel4(currentPanel);
+        break;    
     }
+
+    currentPanel = "";
     
   }
 
-  React.useEffect(() => {
-    sortPanels();
-  }, [ currentPanel1, currentPanel2, currentPanel3, currentPanel4 ]);
+  // React.useEffect(() => {
+  //   sortPanels();
+  // }, [ currentPanel1, currentPanel2, currentPanel3, currentPanel4 ]);
 
   return (
     <div className="home">
@@ -60,7 +98,7 @@ export const Home = () => {
               <select className="home__select" value={panel1} onChange={(e) => {
                 //Calls the handlePanel fucntion to set the current panel selected 
                 const selectedPanel = e.target.value;
-                handlePanel(1, selectedPanel);
+                handleSelectedPanel(1, selectedPanel);
               }} name="panels" id="panels">
                 <option value="select" disabled selected className="home__select-option--disable">Selecciona un panel</option>
                 <option value="exams" className="home__select-option">Exámenes próximos</option>
@@ -77,7 +115,7 @@ export const Home = () => {
               <select className="home__select" value={panel2} onChange={(e) => {
                 //Calls the handlePanel fucntion to set the current panel selected 
                 const selectedPanel = e.target.value;
-                handlePanel(2, selectedPanel);
+                handleSelectedPanel(2, selectedPanel);
               }} name="panels" id="panels">
                 <option value="select" disabled selected className="home__select-option--disable">Selecciona un panel</option>
                 <option value="exams" className="home__select-option">Exámenes próximos</option>
@@ -94,7 +132,7 @@ export const Home = () => {
               <select className="home__select" value={panel3} onChange={(e) => {
                 //Calls the handlePanel fucntion to set the current panel selected 
                 const selectedPanel = e.target.value;
-                handlePanel(3, selectedPanel);
+                handleSelectedPanel(3, selectedPanel);
               }} name="panels" id="panels">
                 <option value="select" disabled selected className="home__select-option--disable">Selecciona un panel</option>
                 <option value="exams" className="home__select-option">Exámenes próximos</option>
@@ -111,7 +149,7 @@ export const Home = () => {
               <select className="home__select" value={panel4} onChange={(e) => {
                 //Calls the handlePanel fucntion to set the current panel selected 
                 const selectedPanel = e.target.value;
-                handlePanel(4, selectedPanel);
+                handleSelectedPanel(4, selectedPanel);
               }} name="panels" id="panels">
                 <option value="select" disabled selected className="home__select-option--disable">Selecciona un panel</option>
                 <option value="exams" className="home__select-option">Exámenes próximos</option>
