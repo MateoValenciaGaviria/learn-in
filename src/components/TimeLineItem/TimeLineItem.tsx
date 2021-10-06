3ced8cde2a6dc91c8aee464d3dce9b347dffec50
Julieth Salazar <Juli.Salazar18@hotmail.com>
2021-10-06 17:37:36 -0500

    Install Firebase and some time line item styles

--- src/components/TimeLineItem/TimeLineItem.tsx
+++ src/components/TimeLineItem/TimeLineItem.tsx
@@ -3,19 +3,19 @@ export const TimeLineItem = () => {
         <div className="time-line-item">
             <h3>Física</h3>
             <p>Unidad 1:Fundamentos básicos de física</p>
-            <div> 
+            <div>
                 <h4>Próxima clase:</h4>
                 <p>Revisión actividad 3</p>
             </div>
-            <div> 
+            <div>
                 <h4>Entregable:</h4>
                 <p>Actividad 3</p>
             </div>
-            <div> 
+            <div>
                 <h4>Estado:</h4>
                 <p>Finalizada</p>
             </div>
-            <div> 
+            <div>
                 <h4>Detalles:</h4>
                 <p>Veremos los conceptos básicos</p>
             </div>
@@ -25,11 +25,12 @@ export const TimeLineItem = () => {
 
 export const TimeLineItemContainer = () => {
     return (
-        <div className="time-line-item-container">
+        <>
             <h5>Lunes 13, Septiembre</h5>
-            <TimeLineItem/>
-            <TimeLineItem/>
-
-        </div>
+            <div className="time-line-item-container">
+                <TimeLineItem />
+                <TimeLineItem />
+            </div>
+        </>
     );
 }
