6b9a06ec89f2ab4fe61599b5573427e69b7d8817
MateoValenciaGaviria <mateo.valencia.g@gmail.com>
2021-10-19 18:52:37 -0500

    Chat section added
    Structure, styles and functionalities

---
+++ src/components/ChatFeed/ChatFeed.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ChatFeed = (props: any) => {
+
+    console.log(props);
+    return (
+      <div className="chatFeed">
+        chat feed
+      </div>
+    );
+  }
--- src/containers/Chat/Chat.tsx
+++ src/containers/Chat/Chat.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import { ChatFeed } from '../../components/ChatFeed/ChatFeed';
 
 export const Chat = () => {
   return (
     <div className="chat">
-      
+      <ChatEngine
+        height= "90vh"
+        projectID="7a20b47a-8235-45bd-a653-6b7379fd4039"
+        userName="Mateo"
+        userSecret="matthew123"
+        //renderChatFeed={(chatAppProps: any) => <ChatFeed {...chatAppProps}></ChatFeed>}
+      ></ChatEngine>
     </div>
   );
 }
