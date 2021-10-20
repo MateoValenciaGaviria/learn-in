7eab6ec9eae097e409f0468ea397eea267806433
MateoValenciaGaviria <mateo.valencia.g@gmail.com>
2021-10-20 00:02:29 -0500

    Chat section styles fixed

--- src/containers/Chat/Chat.tsx
+++ src/containers/Chat/Chat.tsx
@@ -6,6 +6,7 @@ export const Chat = () => {
   return (
     <div className="chat">
       <ChatEngine
+        offset={-5}
         height= "90vh"
         projectID="7a20b47a-8235-45bd-a653-6b7379fd4039"
         userName={localStorage.getItem('username')}
